56edf68f753d2c18ca26ed733137846b41e8682d
ferney <williamchaparro@unisangil.edu.co>
2021-12-08 08:44:40 -0500

    Add files via upload

---
+++ JAVA/Grabación de audio/java/com/example/uno/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.uno;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    int peticion = 1;
+    Uri url1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void grabar(View v) {
+        Intent intent = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+        startActivityForResult(intent, peticion);
+    }
+
+    public void reproducir(View v) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, url1);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == peticion)
+            url1 = data.getData();
+    }
+}
