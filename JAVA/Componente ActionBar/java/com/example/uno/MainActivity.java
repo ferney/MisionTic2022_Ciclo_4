4dda32a46266abdf081be4d1014b2d188bc1ebcb
ferney <williamchaparro@unisangil.edu.co>
2021-12-08 08:42:13 -0500

    Add files via upload

---
+++ JAVA/Componente ActionBar/java/com/example/uno/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.uno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuopciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.opcion1) {
+            Toast.makeText(this,R.string.mensaje_1,Toast.LENGTH_LONG).show();
+        }
+        if (id==R.id.opcion2) {
+            Toast.makeText(this,R.string.mensaje_1,Toast.LENGTH_LONG).show();
+        }
+        if (id==R.id.opcion3) {
+            Toast.makeText(this,R.string.mensaje_1, Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
